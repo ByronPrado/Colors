f8206b0b21293b7d4f06280fdfa71f08585b28e0
ByronPrado <byron.prado@sansano.usm.cl>
2025-04-16 18:04:59 -0400

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Colors;
+using Microsoft.Maui.Controls;
+public partial class MainPage : ContentPage
+{
+	//int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+	private void Slide_change(object sender, ValueChangedEventArgs e)
+	{
+		Value_label.Text= $"Valor: {e.NewValue:0}";
+	}
+
+    private void Change_background_Clicked(object sender, EventArgs e)
+    {
+		
+    }
+}
+
