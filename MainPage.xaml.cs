32582ee73b8239b5d909f8954b2ecabb4137abd6
FranciscoMunozP <147677946+FranciscoMunozP@users.noreply.github.com>
2025-04-23 14:56:01 -0400

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,39 +2,36 @@ namespace Colors;
 using Microsoft.Maui.Controls;
 public partial class MainPage : ContentPage
 {
-	//int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
-	private void Slide_change(object sender, ValueChangedEventArgs e)
+	private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
 	{
-		UpdateBackgroundColor();
+		EstablecerColor();
 	}
 
-    private void Change_background_Clicked(object sender, EventArgs e)
+    private void RandomButton_Clicked(object sender, EventArgs e)
     {
-		 // Generate random RGB values
 		Random random = new Random();
 		int red = random.Next(0, 256);
 		int green = random.Next(0, 256);
 		int blue = random.Next(0, 256);
-		//Update the slider values to the random RGB values
 		R.Value = red;
 		G.Value = green;
 		B.Value = blue;
-		UpdateBackgroundColor();
+		EstablecerColor();
 		}
 
-   private void UpdateBackgroundColor(){
+   private void EstablecerColor(){
 		int red = (int)R.Value;
 		int green = (int)G.Value;
 		int blue = (int)B.Value;
 		Color color = Color.FromRgb(red, green, blue);
 
         BackgroudApp.BackgroundColor = color;
-		Change_background.BackgroundColor = color;
+		RandomButton.BackgroundColor = color;
 		ColorSquare.Color = color;
 
 		string hex = $"#{red:X2}{green:X2}{blue:X2}";
@@ -42,7 +39,7 @@ public partial class MainPage : ContentPage
 
    }
 
-   private async void CopyToClipboard(object sender, EventArgs e)
+   private async void Clipboard_Clicked(object sender, EventArgs e)
 	{
 		await Clipboard.SetTextAsync(Value_label.Text);
 		Label_copied.IsVisible = true;
